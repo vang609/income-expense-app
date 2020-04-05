6129d9ca1e0cd1cee1b600e38d438129050a734a
Enrique Vargas <enrique.vangz@gmail.com>
2020-04-05 12:10:08 -0400

    First commit - Template app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IncomeExpenseComponent } from './income-expense/income-expense.component';
+import { StatisticsComponent } from './income-expense/statistics/statistics.component';
+import { DetailComponent } from './income-expense/detail/detail.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { NavbarComponent } from './share/navbar/navbar.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IncomeExpenseComponent,
+    StatisticsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { StatisticsComponent } from '../income-expense/statistics/statistics.component';
+import { IncomeExpenseComponent } from '../income-expense/income-expense.component';
+import { DetailComponent } from '../income-expense/detail/detail.component';
+
+
+
+export const dashboardRoutes: Routes = [
+    { path: '', component: StatisticsComponent},
+    { path: 'income-expense', component: IncomeExpenseComponent},
+    { path: 'detail', component: DetailComponent},
+]
